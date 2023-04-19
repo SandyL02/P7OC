bf2286f9bcc150f3d1c906d6d23f8410e5e768c8
Sandy Lenormand <sandy.lenormand02@gmail.com>
2023-04-19 11:26:30 +0200

    mise en place des routes

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 const path = require('path');
 
 
-const stuffRoutes = require('./routes/stuff');
+const bookRoutes = require('./routes/book');
 const userRoutes = require('./routes/user');
 
 const app  = express();
@@ -25,7 +25,7 @@ app.use((req, res, next) => {
 });
 
 
-app.use('/api/stuff', stuffRoutes);
+app.use('/api/books', bookRoutes);
 app.use('/api/auth', userRoutes);
 app.use('/images', express.static(path.join(__dirname, 'images')));
 
